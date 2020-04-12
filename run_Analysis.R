9946befa859e941a3f65eb6c62c71c5244ea9eb9
wheygladiator <62173774+wheygladiator@users.noreply.github.com>
2020-04-12 14:42:27 +0200

    Update run_Analysis.R

--- run_Analysis.R
+++ run_Analysis.R
@@ -54,4 +54,4 @@ names(tidy_data) <- gsub("std", "Std", names(tidy_data))
 names(tidy_data) <- gsub("-", "", names(tidy_data))
 
 # output to tidy_data.txt
-write.table(tidy_data, file = "./tidy_data.txt", row.name=FALSE)
+write.table(tidy_data, file = "./tidydata.txt", row.name=FALSE)
